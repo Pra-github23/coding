a9e0accba98d8c88c2df9f91254db379be72ce9d
D.prakash <139935093+Pra-github23@users.noreply.github.com>
2024-12-23 17:23:54 +0530

    no.of provinces
    Given an undirected graph with V vertices. We say two vertices u and v belong to a single province if there is a path from u to v or v to u. Your task is to find the number of provinces.
    
    Note: A province is a group of directly or indirectly connected cities and no other cities outside of the group.

--- problems/graph/no.of provinces.java
+++ problems/graph/no.of provinces.java
@@ -1,30 +1,78 @@
 
 class Solution {
     static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+
+
+        // disjoint approach
         
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        int p[] = new int[V];
+        int l[] = new int[V];
         
-        for(int i =0; i<adj.size(); i++)
-         res.add(new ArrayList<>());
+        for(int i =0; i < V; i++){
+            p[i] = i;
+            
+        }
+        
+        for(int i =0; i< V; i++){
+            for(int j =0; j < V; j++){
+                if(i != j && adj.get(i).get(j) == 1){
+                    
+                    union(p,l, i, j);
+                }
+            }
+        }
+        int c = 0;
+        for(int i =0; i< V; i++)
+        if(p[i] == i)c++;
+        
+        // bfs approach
+        
+        // ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        
+        // for(int i =0; i<adj.size(); i++)
+        //  res.add(new ArrayList<>());
          
-        for(int i = 0; i < adj.size(); i++){
-            for(int j = 0; j < adj.size(); j++){
+        // for(int i = 0; i < adj.size(); i++){
+        //     for(int j = 0; j < adj.size(); j++){
                 
-                if(adj.get(i).get(j) == 1) res.get(i).add(j);
-            }
-        }
+        //         if(adj.get(i).get(j) == 1) res.get(i).add(j);
+        //     }
+        // }
          
-        int v[] = new int[V];
-        int c =0;
+        // int v[] = new int[V];
+        // int c =0;
         
-        for(int i =0; i<V; i++){
-            if(v[i] != 1){
-                // System.out.println("yes");
-                c++;
-                bfs(v, res, i);
+        // for(int i =0; i<V; i++){
+        //     if(v[i] != 1){
+        //         // System.out.println("yes");
+        //         c++;
+        //         bfs(v, res, i);
+        //     }
+        // }
+        return c;
+    }
+    private static int parent(int[]p ,int ind){
+        if(p[ind] == ind)return ind;
+        
+        return p[ind] = parent(p, p[ind]);
+    }
+    
+    private static void union(int[] p, int[] l, int s, int d){
+        int pS = parent(p, s);
+        int pD = parent(p, d);
+        
+        if(pS != pD){
+            if(l[pS] > l[pD]){
+                p[pD] = pS;
+            }
+            else if(l[pS] < l[pD]){
+                p[pS] = pD;
+            }
+            else{
+                p[pD] = pS;
+                l[pS]++;
             }
         }
-        return c;
     }
     private static void bfs(int v[], ArrayList<ArrayList<Integer>> adj, int i){
         
