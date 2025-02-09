526984434efc73be0fddd51d01cf481b974a3fba
D.prakash <139935093+Pra-github23@users.noreply.github.com>
2025-02-09 10:51:11 +0530

    Pass by Reference and Value
    Geek is learning about functions and calling a function with arguments. He learns that passing can take one of two forms: pass by value or pass by reference.
    
    Geek wishes to add 1 and 2, respectively, to the parameter passed by value and reference. Help Geek in fulfilling his goal.

---
+++ python/basics/Pass by Reference and Value.py
@@ -0,0 +1,10 @@
+import array
+class Solution:
+    def passedBy(self, a, b):
+        # Code here
+        ar = array.array('i',[a+1, b+2])
+        
+        # ar =[a+1, b+2]
+        # return ar
+        
+        return ar
